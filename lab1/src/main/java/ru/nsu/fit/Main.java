34a31438906e430e99a340c79e0b6c9155401962
Nikita Melnikov <114857730+n1klze@users.noreply.github.com>
2023-03-05 21:37:41 +0700

    [lab1] Add file logger.

--- lab1/src/main/java/ru/nsu/fit/Main.java
+++ lab1/src/main/java/ru/nsu/fit/Main.java
@@ -1,7 +1,23 @@
 package ru.nsu.fit;
 
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    static {
+        try {
+            InputStream stream = Main.class.getClassLoader().getResourceAsStream("logging.properties");
+            LogManager.getLogManager().readConfiguration(stream);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOGGER.log(Level.INFO, "Launch calculator application.");
     }
 }
