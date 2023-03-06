0291dfb5ec480dc6a2486077213114e486b6e0de
Nikita Melnikov <114857730+n1klze@users.noreply.github.com>
2023-03-07 00:09:17 +0700

    [lab1] Update Command Line Parser.

--- lab1/src/main/java/ru/nsu/fit/CommandLineParser.java
+++ lab1/src/main/java/ru/nsu/fit/CommandLineParser.java
@@ -5,17 +5,18 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class CommandLineParser {
-    private InputStream in = System.in;
+    private InputStream in;
 
     public void parse(String[] args) {
         if (args == null || args.length == 0) {
+            in = System.in;
             return;
         }
         String fileName = args[0];
         try {
             in = new FileInputStream(fileName);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (FileNotFoundException except) {
+            throw new RuntimeException(except);
         }
     }
 
