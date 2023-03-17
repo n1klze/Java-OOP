09987da42443a827f4bcdc197467b8942ab02ac9
Nikita Melnikov <114857730+n1klze@users.noreply.github.com>
2023-03-17 15:06:44 +0700

    [lab1] Add * command.

---
+++ lab1/src/main/java/ru/nsu/fit/commands/Multiplication.java
@@ -0,0 +1,17 @@
+package ru.nsu.fit.commands;
+
+import ru.nsu.fit.Context;
+import ru.nsu.fit.exceptions.StackSizeException;
+
+import java.util.Deque;
+
+public class Multiplication implements Command {
+    @Override
+    public void exec(String[] args, Context executionContext) throws StackSizeException{
+        Deque<Double> stack = executionContext.getStack();
+
+        if (stack.size() < 2) throw new StackSizeException();
+
+        stack.push(stack.pop() * stack.pop());
+    }
+}
