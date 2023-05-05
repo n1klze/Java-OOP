7da9f4fc3a894350b57dea5518ce92ce1f14ad47
n.melnikov1 <n.melnikov1@g.nsu.ru>
2023-05-05 17:43:25 +0700

    [lab2] Add custom font.

--- lab2/src/main/java/ru/nsu/ccfit/melnikov/minesweeper/view/gui/components/MenuButton.java
+++ lab2/src/main/java/ru/nsu/ccfit/melnikov/minesweeper/view/gui/components/MenuButton.java
@@ -3,19 +3,33 @@ package ru.nsu.ccfit.melnikov.minesweeper.view.gui.components;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.io.IOException;
 
 public class MenuButton extends JButton {
-    private static final int FONT_SIZE = 20;
+    private static final String FONT_PATH = "/pixel_font.ttf";
+    private static final Font FONT;
+    private static final float FONT_SIZE = 20;
+
+    static {
+        try {
+            Font nonSizedFont = Font.createFont(Font.TRUETYPE_FONT,
+                    MenuButton.class.getResourceAsStream(FONT_PATH));
+            FONT = nonSizedFont.deriveFont(FONT_SIZE);
+        } catch (FontFormatException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public MenuButton(String text) {
         super();
 
         setText(text);
+        setFont(FONT);
         //super.setContentAreaFilled(false);
         setFocusPainted(false);
         setBackground(Color.CYAN);
         setBorder(new LineBorder(Color.DARK_GRAY, 1));
-        setFont(new Font(Font.DIALOG, Font.BOLD, FONT_SIZE));
+        //setFont(new Font(Font.DIALOG, Font.BOLD, FONT_SIZE));
         //setForeground(Color.WHITE);
     }
 }
